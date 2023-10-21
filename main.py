197c9bf0afa64987754b6479d17003194fc931d9
Rob <ravenstudiosrob@yahoo.com>
2023-10-20 23:37:09 -0500

    Revert "Jordan"

--- main.py
+++ main.py
@@ -1,26 +1,17 @@
-import pygame
-import random
-import object
 from constants import *
+import pygame
+import state_manager
 
 
-pygame.init()
 clock = pygame.time.Clock()
 surface = pygame.display.set_mode((GAME_WIDTH, GAME_HEIGHT))
 
-player_group = pygame.sprite.Group()
-object_group = pygame.sprite.Group()
 
-number_candy = random.randint(0, 100)
-for _ in range(number_candy):
-    random_x = random.randint(0, GAME_WIDTH)
-    obj = object.Object(random_x, 0)
-    object_group.add(obj)
-for o in object_group:
 
 
 pygame.init()
 
+sm = state_manager.State_manager()
 
 def main():
     running = True
@@ -30,22 +21,22 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        update()
         draw()
+        update()
 
     pygame.quit()
 
 
+
 def draw():
     surface.fill((0, 0, 10))#background
-    player_group.draw(surface)
-    object_group.draw(surface)
+    sm.draw(surface)
     pygame.display.flip()
 
 
 def update():
-    player_group.update()
-    object_group.update()
+    sm.update()
+
 
 
 if __name__ == "__main__":
